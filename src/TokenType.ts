d15950e0be9523a3a381e046d492034128e023e2
barleto <riczcharf@gmail.com>
2025-04-19 10:52:24 +0100

    Separating in different files

---
+++ src/TokenType.ts
@@ -0,0 +1,15 @@
+export enum TokenType {
+    EOF,
+    LITERAL,
+    IGNORE_RULE,
+    OR,
+    ARROW,
+    RULE,
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    PLUS,
+    STAR,
+    SEMI_COL,
+    ONE_LINE_COMM,
+    MULTI_LINE_COMM
+}
